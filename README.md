965d267742259c7724ae5de28bf6b0392da12218
Niklas K <drdaxxy@googlemail.com>
2018-05-22 05:11:31 +0200

    done basically

---
+++ README.md
@@ -0,0 +1,11 @@
+# REEEEEEGen
+
+This is dumb.
+
+- Cell sizes and padding are hardcoded
+- Charset is C;C from SciAdv.Net, same build we use for sc3tools in cc-patch
+- **Compound characters are not supported**
+- Font textures and widths are embedded
+- **Current font textures are not the same as the ones I have in my dev install** (not sure which are the right ones)
+- We're replacing them after mipmapping anyway
+- To use, put all lines in an `input.txt` in working directory, `line%d.png` files will be generated.
--- REEEEEEGen/Program.cs
+++ REEEEEEGen/Program.cs
@@ -1,5 +1,11 @@
-﻿using System;
+﻿using Imaging.DDSReader;
+using SciAdvNet.SC3Script;
+using SciAdvNet.SC3Script.Text;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +16,117 @@ namespace REEEEEEGen
     {
         static void Main(string[] args)
         {
+            var game = SC3Game.ChaosChild;
+            var charset = new CharacterSet(game, "");
+
+            const int outlinePadding = 4;
+            const int cellWidth = 32;
+            const int cellHeight = 32;
+            const int outlineCellWidth = 38;
+            const int outlineCellHeight = 38;
+            const double scaleFactor = 1.5;
+            const int colCount = 64;
+
+            var fontA = DDS.LoadImage(Assets.FONT_A);
+            var fontB = DDS.LoadImage(Assets.FONT_B);
+            var fontOutlineA = DDS.LoadImage(Assets.font_outline_A);
+            var fontOutlineB = DDS.LoadImage(Assets.font_outline_B);
+
+            var font = new Bitmap(fontA.Width, fontA.Height + fontB.Height);
+            var fontOutline = new Bitmap(fontOutlineA.Width, fontOutlineA.Height + fontOutlineB.Height);
+
+            var fontGraphics = Graphics.FromImage(font);
+            fontGraphics.CompositingMode = System.Drawing.Drawing2D.CompositingMode.SourceCopy;
+            fontGraphics.DrawImage(fontA, new Point(0, 0));
+            fontGraphics.DrawImage(fontB, new Point(0, fontA.Height));
+            var fontOutlineGraphics = Graphics.FromImage(fontOutline);
+            fontOutlineGraphics.CompositingMode = System.Drawing.Drawing2D.CompositingMode.SourceCopy;
+            fontOutlineGraphics.DrawImage(fontOutlineA, new Point(0, 0));
+            fontOutlineGraphics.DrawImage(fontOutlineB, new Point(0, fontOutlineA.Height));
+
+            float[][] colorMatrixElements = {
+               new float[] {0,  0,  0,  0, 0},
+               new float[] {0,  0,  0,  0, 0},
+               new float[] {0,  0,  0,  0, 0},
+               new float[] {0,  0,  0,  1, 0},
+               new float[] {0, 0, 0, 0, 1}};
+
+            ColorMatrix colorMatrix = new ColorMatrix(colorMatrixElements);
+
+            var outlineAttrs = new ImageAttributes();
+            var fontAttrs = new ImageAttributes();
+            outlineAttrs.SetColorMatrix(
+               colorMatrix,
+               ColorMatrixFlag.Default,
+               ColorAdjustType.Bitmap);
+
+            int lineId = 0;
+            string line;
+            StreamReader reader = new StreamReader("input.txt");
+            while ((line = reader.ReadLine()) != null)
+            {
+
+                int totalWidth = 32;
+                int totalHeight = 72;
+                int curX = 16;
+                int curY = 10;
+
+                foreach (char c in line)
+                {
+                    ushort charId = (ushort)(charset.EncodeCharacter(c) & 0x7FFF);
+                    totalWidth += (int)Math.Round(Assets.widths[charId] * scaleFactor);
+                }
+
+                var output = new Bitmap(totalWidth, totalHeight);
+                var outputGraphics = Graphics.FromImage(output);
+                outputGraphics.CompositingMode = System.Drawing.Drawing2D.CompositingMode.SourceOver;
+
+                foreach (char c in line)
+                {
+                    ushort charId = (ushort)(charset.EncodeCharacter(c) & 0x7FFF);
+                    int row = charId / colCount;
+                    int col = charId % colCount;
+
+                    int glyphWidth = (int)Math.Round(Assets.widths[charId] * scaleFactor);
+
+                    var outlineDestRect = new Rectangle(
+                        curX - outlinePadding,
+                        curY - outlinePadding,
+                        (int)(outlineCellWidth * scaleFactor),
+                        (int)(outlineCellHeight * scaleFactor));
+
+                    var destRect = new Rectangle(
+                        curX,
+                        curY,
+                        (int)(cellWidth * scaleFactor),
+                        (int)(cellHeight * scaleFactor));
+
+                    outputGraphics.DrawImage(
+                        fontOutline,
+                        outlineDestRect,
+                        (int)(col * outlineCellWidth * scaleFactor),
+                        (int)(row * outlineCellHeight * scaleFactor),
+                        (int)(outlineCellWidth * scaleFactor),
+                        (int)(outlineCellHeight * scaleFactor),
+                        GraphicsUnit.Pixel,
+                        outlineAttrs);
+
+                    outputGraphics.DrawImage(
+                        font,
+                        destRect,
+                        (int)(col * cellWidth * scaleFactor),
+                        (int)(row * cellHeight * scaleFactor),
+                        (int)(cellWidth * scaleFactor),
+                        (int)(cellHeight * scaleFactor),
+                        GraphicsUnit.Pixel,
+                        fontAttrs);
+
+                    curX += glyphWidth;
+                }
+
+                output.Save(string.Format("line{0}.png", lineId));
+                lineId++;
+            }
         }
     }
 }
